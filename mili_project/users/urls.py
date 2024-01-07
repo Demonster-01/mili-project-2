7c80a1a3c78ac9cf8c9ab9c33e704fa11473e984
Suresh <kac2035@gmail.com>
2024-01-07 22:18:00 +0545

    register add

--- mili_project/users/urls.py
+++ mili_project/users/urls.py
@@ -7,5 +7,6 @@ app_name = 'user'
 urlpatterns = [
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('login/',  views.login, name='login'),
+    path('register/',views.register,name="register"),
 
 ]
